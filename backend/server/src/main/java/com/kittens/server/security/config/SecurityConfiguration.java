22fc345deff0ca06cd894d2f5158f1eeee417c7b
phoekoby <phoekoby@mail.ru>
2023-12-11 16:00:12 +0300

    Убрал CORS

--- backend/server/src/main/java/com/kittens/server/security/config/SecurityConfiguration.java
+++ backend/server/src/main/java/com/kittens/server/security/config/SecurityConfiguration.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,4 +38,14 @@ public class SecurityConfiguration {
                 .build();
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
